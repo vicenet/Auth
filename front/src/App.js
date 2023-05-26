201ca9a524defd18dc638a2d588f1b9348afcda3
vicenet <gitaubrian425@gmail.com>
2023-05-26 07:35:07 +0300

    correction

--- front/src/App.js
+++ front/src/App.js
@@ -1,39 +1,13 @@
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
 import Modal from 'react-modal';
-import { Button } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
 import Register from './components/Register';
 import Login from './components/Login';
 import './App.css';
 
 Modal.setAppElement('#root'); // Set the root element for the modal
 
-const useStyles = makeStyles((theme) => ({
-  navbar: {
-    display: 'flex',
-    justifyContent: 'center',
-    marginBottom: theme.spacing(2),
-  },
-  navLink: {
-    margin: theme.spacing(0, 2),
-    color: '#333',
-    textDecoration: 'none',
-  },
-  modalContent: {
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    backgroundColor: theme.palette.background.paper,
-    boxShadow: theme.shadows[5],
-    padding: theme.spacing(4),
-    outline: 'none',
-  },
-}));
-
 const App = () => {
-  const classes = useStyles();
   const [isRegisterModalOpen, setRegisterModalOpen] = useState(false);
   const [isLoginModalOpen, setLoginModalOpen] = useState(false);
 
@@ -55,35 +29,21 @@ const App = () => {
 
   return (
     <Router>
-      <div className={classes.navbar}>
-        <Link to="/" className={classes.navLink} onClick={openRegisterModal}>
-          Register
-        </Link>
-        <Link to="/login" className={classes.navLink} onClick={openLoginModal}>
+      <div className="navbar">
+        <Link to="/login" className="nav-link" onClick={openLoginModal}>
           Login
         </Link>
       </div>
       <Routes>
-        <Route exact path="/" component={Register} />
-        <Route path="/login" component={Login} />
+        <Route path="/" element={<Register />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
-
-      {/* Register Modal */}
-      <Modal
-        isOpen={isRegisterModalOpen}
-        onRequestClose={closeRegisterModal}
-        contentLabel="Register Modal"
-        className={classes.modalContent}
-      >
-        <Register />
-      </Modal>
 
       {/* Login Modal */}
       <Modal
         isOpen={isLoginModalOpen}
         onRequestClose={closeLoginModal}
         contentLabel="Login Modal"
-        className={classes.modalContent}
       >
         <Login />
       </Modal>
